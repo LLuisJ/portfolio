492f09fed92150d81ab0adbfe6de847d45aee07b
Luis Jäger <jaeger_luis@web.de>
2025-01-15 20:53:10 +0100

    Projects Component angepasst

--- components/projects.tsx
+++ components/projects.tsx
@@ -18,22 +18,39 @@ import {
 import { Button } from "./ui/button";
 import { SquareArrowOutUpRight } from "lucide-react";
 
+interface Project {
+    title: string;
+    description: string;
+    link: string;
+}
+
+interface ProjectsObject {
+    [key: string]: Array<Project>
+}
+
 export default function Projects() {
-    const projects = {
-        go: [
+    const projects: ProjectsObject = {
+        "Go": [
             {
                 title: "ch",
                 description: "A small tool for creating/checking hashes",
                 link: "https://github.com/LLuisJ/ch",
             },
         ],
-        odin: [
+        "Odin": [
             {
                 title: "OBF",
                 description: "A Brainfuck compiler written in Odin",
                 link: "https://github.com/LLuisJ/OBF",
             },
         ],
+        "Web": [
+            {
+                title: "Portfolio",
+                description: "This website here",
+                link: "https://github.com/LLuisJ/portfolio"
+            }
+        ]
     };
 
     return (
@@ -42,86 +59,51 @@ export default function Projects() {
                 My Projects so far on Github
             </div>
             <Tabs defaultValue="go" className="w-[300px] sm:w-[500px] mx-auto">
-                <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="go">Go</TabsTrigger>
-                    <TabsTrigger value="odin">Odin</TabsTrigger>
+                <TabsList className="grid w-full grid-cols-3">
+                    {Object.keys(projects).map((key) => (
+                        <TabsTrigger value={key}>{key}</TabsTrigger>
+                    ))}
                 </TabsList>
-                <TabsContent value="go">
-                    <Carousel className="w-full max-w">
-                        <CarouselContent>
-                            {projects.go.map((item, k) => (
-                                <CarouselItem key={k}>
-                                    <div className="p-1">
-                                        <Card>
-                                            <CardHeader>
-                                                <CardTitle>
-                                                    {item.title}
-                                                </CardTitle>
-                                                <CardDescription>
-                                                    {item.description}
-                                                </CardDescription>
-                                            </CardHeader>
-                                            <CardContent>
-                                                <Button
-                                                    variant="outline"
-                                                    onClick={() =>
-                                                        window.open(
-                                                            item.link,
-                                                            "_blank"
-                                                        )
-                                                    }
-                                                >
-                                                    <SquareArrowOutUpRight />
-                                                    {item.link}
-                                                </Button>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                            ))}
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-                </TabsContent>
-                <TabsContent value="odin">
-                    <Carousel className="w-full max-w">
-                        <CarouselContent>
-                            {projects.odin.map((item, k) => (
-                                <CarouselItem key={k}>
-                                    <div className="p-1">
-                                        <Card>
-                                            <CardHeader>
-                                                <CardTitle>
-                                                    {item.title}
-                                                </CardTitle>
-                                                <CardDescription>
-                                                    {item.description}
-                                                </CardDescription>
-                                            </CardHeader>
-                                            <CardContent>
-                                                <Button
-                                                    variant="outline"
-                                                    onClick={() =>
-                                                        window.open(
-                                                            item.link,
-                                                            "_blank"
-                                                        )
-                                                    }
-                                                >
-                                                    <SquareArrowOutUpRight />
-                                                    {item.link}
-                                                </Button>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                            ))}
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-                </TabsContent>
+                {Object.keys(projects).map((key) => (
+                    <TabsContent value={key}>
+                        <Carousel className="w-full max-w">
+                            <CarouselContent>
+                                {projects[key].map((item, k) => (
+                                    <CarouselItem key={k}>
+                                        <div className="p-1">
+                                            <Card>
+                                                <CardHeader>
+                                                    <CardTitle>
+                                                        {item.title}
+                                                    </CardTitle>
+                                                    <CardDescription>
+                                                        {item.description}
+                                                    </CardDescription>
+                                                </CardHeader>
+                                                <CardContent>
+                                                    <Button
+                                                        variant="outline"
+                                                        onClick={() =>
+                                                            window.open(
+                                                                item.link,
+                                                                "_blank"
+                                                            )
+                                                        }
+                                                    >
+                                                        <SquareArrowOutUpRight />
+                                                        {item.link}
+                                                    </Button>
+                                                </CardContent>
+                                            </Card>
+                                        </div>
+                                    </CarouselItem>
+                                ))}
+                            </CarouselContent>
+                            <CarouselPrevious />
+                            <CarouselNext />
+                        </Carousel>
+                    </TabsContent>
+                ))}
             </Tabs>
         </div>
     );
