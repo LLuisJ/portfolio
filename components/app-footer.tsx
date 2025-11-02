b05292f7fff8bbc43f3b40fd929c450f5a8448f5
LLuisJ <jaeger_luis@web.de>
2025-11-02 15:17:07 +0100

    simpler footer

--- components/app-footer.tsx
+++ components/app-footer.tsx
@@ -26,37 +26,14 @@ export default function Footer() {
 
     return (
         <div className="w-full py-10 lg:py-20 bg-background text-foreground">
-            <div className="container ml-5">
-                <div className="grid lg:grid-cols-2 gap-10 items-center">
-                    <div className="flex gap-8 flex-col items-start">
-                        <div className="flex gap-2 flex-col">
-                            <h2 className="text-xl md:text-2xl tracking-tighter max-w-xl font-regular text-left">
-                                Luis Jäger, 2025
-                            </h2>
-                            <p className="text-l max-w-lg leading-relaxed tracking-tight text-foreground/75 text-left">
-                                Let&apos;s see how this goes...
-                            </p>
-                        </div>
-                    </div>
-                    <div className="grid lg:grid-cols-4 gap-10 items-start">
-                        {navigationItems.map((item) => (
-                            <div
-                                key={item.title}
-                                className="flex text-base gap-1 flex-col items-start"
-                            >
-                                <div className="flex flex-col gap-2">
-                                    <Link
-                                        href={item.href}
-                                        className="flex justify-between items-center"
-                                    >
-                                        <span className="text-l">
-                                            {item.title}
-                                        </span>
-                                    </Link>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+            <div className="w-full flex justify-center">
+                <div className="flex flex-col gap-4 items-center text-center">
+                    <h2 className="text-xl md:text-2xl tracking-tighter font-regular">
+                        Luis Jäger, 2025
+                    </h2>
+                    <p className="text-l max-w-lg leading-relaxed tracking-tight text-foreground/75">
+                        Let&apos;s see how this goes...
+                    </p>
                 </div>
             </div>
         </div>
