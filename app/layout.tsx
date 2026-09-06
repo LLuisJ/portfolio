55f139ce3134d274b22e6fd44339d8326f0300e6
LLuisJ <jaeger_luis@web.de>
2026-09-06 18:00:16 +0200

    some fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,6 @@ import Header from "@/components/app-header";
 import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "@/components/app-footer";
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
-
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
     variable: "--font-geist-sans",
